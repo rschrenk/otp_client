1f7565ca5256db57306d27ab92048a71d8ac2cbd
Robert Schrenk <robert.schrenk@bmbwf.gv.at>
2025-04-08 08:15:47 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron/main')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+  // Open Chrome debugging
+  win.webContents.openDevTools()
+  // Open viewer
+  win.loadFile('index.html')
+  BrowserWindow.$ = BrowserWindow.jQuery = require('jquery')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
