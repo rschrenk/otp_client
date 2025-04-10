34dade2769968d76ccfa6360e7fcaf39ee49bee7
Robert Schrenk <robert@schrenk.cc>
2025-04-10 18:59:56 +0200

    Convert to standalone HTML5-Webapp

---
+++ js/connector.js
@@ -0,0 +1,104 @@
+const CONNECTOR = {
+    headers: function() {
+        let user = CONFIG.get('user');
+        let pass = CONFIG.get('pass');
+        return {
+            "Authorization": 'Basic ' + btoa(user + ":" + pass),
+            "OCS-APIRequest": "true"
+        };
+    },
+    load_accounts: function() {
+        console.log('CONNECTOR.load_accounts()');
+        let headers = this.headers();
+        let server = CONFIG.get('server');
+        try {
+            $.ajax({
+                url: server + "/index.php/apps/otpmanager/accounts",
+                data: {
+                    credentials: "omit"
+                },
+                headers: headers
+            }).done(function(data) {
+                data.accounts.forEach(function(account){
+                    console.log(account);
+                    account.currentotp = CONNECTOR.getOtpFromAccount(account);
+                });
+                OTPUI.print_accounts(data.accounts);
+            });
+        } catch {
+            OTPUI.set_result("Error loading: " + server + "/index.php/apps/otpmanager/accounts");
+            return false;
+        }
+    },
+    load_iv: function() {
+        console.log('CONNECTOR.load_iv()');
+        let headers = this.headers();
+        let otppass = CONFIG.get('otppass');
+        let server = CONFIG.get('server');
+        headers['Content-Type'] = "application/json";
+        try {
+            $.ajax({
+                method: 'POST',
+                url: server + "/ocs/v2.php/apps/otpmanager/password/check",
+                data: JSON.stringify({password: otppass}),
+                headers: headers
+            }).done(function(data) {
+                if (typeof data.iv === "undefined") {
+                    OTPUI.set_result('Initialization Vector could not be loaded. Cannot generate OTP-codes!');
+                } else {
+                    CONFIG.set('iv', data.iv);
+                    CONFIG.store();
+                }
+            });
+        } catch {
+            OTPUI.set_result("Error loading: " + server + "/ocs/v2.php/apps/otpmanager/password/check");
+            OTPUI.toggle_page('config');
+            return false;
+        }
+    },
+    getAlgorithm: function(n) {
+        return ["SHA1", "SHA256", "SHA512"][n];
+    },
+    getOtpFromAccount: function(account) {
+        const passHash = CryptoJS.SHA256(CONFIG.get('otppass')).toString();
+        const key = CryptoJS.enc.Hex.parse(passHash);
+        const parsedIv = CryptoJS.enc.Hex.parse(CONFIG.get('iv'));
+        const dec = CryptoJS.AES.decrypt(account.secret, key, { iv: parsedIv });
+        const secret = dec.toString(CryptoJS.enc.Utf8);
+
+        if (account.type == "totp") {
+            const totp = new OTPAuth.TOTP({
+                issuer: account.issuer,
+                label: account.name,
+                algorithm: this.getAlgorithm(account.algorithm),
+                digits: account.digits,
+                period: account.period,
+                secret: secret,
+            });
+            return totp.generate();
+        } else {
+            if (account.counter == 0) {
+                account.code = "Click the button to generate HOTP code";
+            } else {
+                const hotp = new OTPAuth.HOTP({
+                    issuer: account.issuer,
+                    label: account.name,
+                    algorithm: this.getAlgorithm(account.algorithm),
+                    digits: account.digits,
+                    counter: account.counter,
+                    secret: secret,
+                });
+                return hotp.generate();
+            }
+        }
+    },
+    reload_counter: function() {
+        let counter = parseInt($('#reload-counter').html())-1;
+        let period = parseInt($('#reload-counter').data('period'));
+        if (counter <= 0) {
+            counter = period;
+            CONNECTOR.load_accounts();
+        }
+        $('#reload-counter').html(counter);
+    }
+}
---
+++ js/otpui.js
@@ -0,0 +1,52 @@
+const OTPUI = {
+    copy_to_clipboard: function(a) {
+        var content = $(a).find('.otpcode').html();
+        navigator.clipboard.writeText(content)
+            .then(function() {
+                OTPUI.highlight(a, 'alert-success', 1000);
+            })
+            .catch(function(){
+                OTPUI.highlight(a, 'alert-warning', 1000);
+            });
+    },
+    /**
+     * Highlight a certain object by use of css classes for a certain period.
+     * @param obj
+     * @param classes
+     * @param timeout
+     */
+    highlight: function(obj, classes, timeout) {
+        $(obj).addClass(classes);
+        setTimeout(function() { $(obj).removeClass(classes)}, timeout);
+    },
+    print_accounts: function(accounts) {
+        let container = $('#accounts').empty();
+        let group = $('<div>').addClass('list-group');
+        accounts.forEach(function(account) {
+            let a = $('<a>').attr('href', '#').attr('onclick', 'OTPUI.copy_to_clipboard(this);').addClass('alert list-item');
+            let item = $('<div>').addClass('d-flex w-100 justify-content-between');
+            item.append($('<p>').addClass('mb-1 issuer').html(account.issuer));
+            item.append($('<small>').addClass('otpcode').html(account.currentotp));
+            a.append(item);
+            group.append(a);
+        });
+        container.append(group);
+    },
+    /**
+     * Show an alert.
+     * @param result
+     * @param alerttype warning, success, ... in case no alerttype is specified, set to "warning".
+     */
+    set_result: function(result, alerttype) {
+        $('#main-alert').empty();
+        if (typeof alerttype === 'undefined') {
+            alerttype = 'warning';
+        }
+        // Remove all other classes and set only the desired alerttype.
+        $('#main-alert').set('class', 'alert').addClass('alert-' + alerttype).html(result);
+    },
+    toggle_page: function(pagetoopen) {
+        $('[role=page]').addClass('hidden');
+        $('#' + pagetoopen).removeClass('hidden');
+    }
+}
